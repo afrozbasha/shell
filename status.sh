0c8a9fe5cdcbdd98a547e4828ae09cc0a17f1edf
Afroz Basha Shaik Kanala <80997964+afrozbasha@users.noreply.github.com>
2022-11-23 12:00:36 +0530

    Update status.sh

--- status.sh
+++ status.sh
@@ -4,11 +4,9 @@ status=$(helm status nginx | grep "STATUS")
 temp="STATUS: deployed"
 #Check equality two string variables
 
-if [$status==$temp]; then
+if [$status==$temp] 
+then
           echo "Helm chat is deployed"
-          helm status nginx | grep "STATUS"
-
-  else
-            echo "Helm chat is not deployed"
-            helm delete nginx
-    fi
+else
+          echo "Helm chat is not deployed"
+fi
