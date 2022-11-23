085a800f362a49705d8a872dbbe77305e7dc7be4
Afroz Basha Shaik Kanala <80997964+afrozbasha@users.noreply.github.com>
2022-11-23 12:20:34 +0530

    Update cell.sh

--- cell.sh
+++ cell.sh
@@ -3,7 +3,7 @@ status=$(helm status nginx | grep "STATUS")
 #Pass the variable in string
 case "$status" in
 	#case 1
-	"STATUS: deployed") heml status nginx | grep "STATUS" ;;
+	"STATUS: deployed") helm status nginx | grep "STATUS" ;;
 	
 	#case 2
 	"STATUS: pending") helm delete nginx ;;
